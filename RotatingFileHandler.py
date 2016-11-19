2c13cd6f9948f61ab65684b15ad5c17bae3fd7fd
sid802 <sid802@users.noreply.github.com>
2016-11-19 23:38:05 +0200

    Added option for directory creating if non-existent

--- RotatingFileHandler.py
+++ RotatingFileHandler.py
@@ -13,7 +13,9 @@ class RotatingFileHandler(handlers.RotatingFileHandler):
         super(RotatingFileHandler, self).__init__(filename, *args, **kwargs)
         abs_path = os.path.abspath(target_dir)
         if not os.path.isdir(abs_path):
-            raise Exception("You didn't enter a valid target directory!: <{0}>".format(target_dir))
+            dir_created = self._try_create_dir(abs_path)
+            if not dir_created:
+                raise Exception("You didn't enter a valid target directory!: <{0}>".format(target_dir))
         self.target_dir = abs_path
         self.filename = os.path.basename(filename)
 
@@ -25,17 +27,31 @@ class RotatingFileHandler(handlers.RotatingFileHandler):
             self.stream.close()
             self.stream = None
         if self.backupCount > 0:
-            for i in range(self.backupCount - 1, 0, -1):
-                sfn = self.rotation_filename("%s/%s.%d" % (self.target_dir, self.filename, i))  # sfn = source file name
-                dfn = self.rotation_filename("%s/%s.%d" % (self.target_dir, self.filename,  # dfn = destiny file name
-                                                           i + 1))
+             for i in range(self.backupCount - 1, 0, -1):
+                sfn = "%s/%s.%d" % (self.target_dir, self.filename, i)
+                dfn = "%s/%s.%d" % (self.target_dir, self.filename, i + 1)
                 if os.path.exists(sfn):
                     if os.path.exists(dfn):
                         os.remove(dfn)
                     os.rename(sfn, dfn)
-            dfn = self.rotation_filename(os.path.join(self.target_dir, self.filename + ".1"))
-            if os.path.exists(dfn):
-                os.remove(dfn)
-            self.rotate(self.baseFilename, dfn)
+                dfn = os.path.join(self.target_dir, self.filename + ".1")
+             if os.path.exists(dfn):
+                 os.remove(dfn)
+             os.rename(os.path.join(self.target_dir, self.baseFilename), dfn)
         if not self.delay:
             self.stream = self._open()
+
+    def _try_create_dir(self, target_dir):
+        """
+        :param target_dir: Absolute path to wanted dir
+        Prompts the user if he want to create this dir
+        :return: boolean - directory has been created or not
+        """
+        user_input = raw_input("The directory `{0}` doesn't exist, do you want it to be created? Y/N: ".format(target_dir))
+        while user_input.lower() not in ['y', 'n']:
+            user_input = raw_input("The directory `{0}` doesn't exist, do you want it to be created? Y/N: ".format(target_dir))
+
+        if user_input == 'y':
+            os.makedirs(target_dir)
+            return True
+        return False
